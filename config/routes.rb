477d5f235bb62049678f0bb8c9315858c419c862
kenta.sasaki <sasakikenntaqwer@gmail.com>
2020-06-07 15:03:18 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/index'
   get 'users/show'
   get 'users/new'
@@ -10,5 +13,9 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
