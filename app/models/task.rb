07256f99d2f1bca89b1316304fa431e6771c4482
kenta.sasaki <sasakikenntaqwer@gmail.com>
2020-06-13 00:30:44 +0000

    heroku0613

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,11 @@
 class Task < ApplicationRecord
+  
+ belongs_to :user
+   
  validates :status, presence: true, length: { maximum: 10 }
  validates :content, presence: true, length: { maximum: 255 }
  
- belongs_to :user
+
  
  
 end
